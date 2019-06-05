d6b95f55ae2132a204b98eeca32bbfbfe773135a
sandeep <sandy@gmail.com>
2019-06-05 16:15:08 +0530

    2nd commit

--- src/main/java/pack1/Application.java
+++ src/main/java/pack1/Application.java
@@ -10,6 +10,7 @@ public class Application
 	public static void main(String[] args)
 	{
 		//Demanistration of Spring MVC boot
+// sandeep code new
 		SpringApplication.run(Application.class, args);
 	}
 }
